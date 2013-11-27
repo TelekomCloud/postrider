a74e340df0f59c8e25b0baa83688363e9f943f14
Dominik Richter <dominik.richter@gmail.com>
2013-11-27 12:26:09 +0100

    added missing angular animate to test
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- karma.conf.js
+++ karma.conf.js
@@ -33,6 +33,7 @@ module.exports = function(config) {
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/bower_components/angular-cookies/angular-cookies.js',
       'app/bower_components/angular-sanitize/angular-sanitize.js',
+      'app/bower_components/angular-animate/angular-animate.min.js',
       'app/bower_components/lodash/dist/lodash.min.js',
       'app/bower_components/restangular/dist/restangular.min.js',
       'app/scripts/*.js',
